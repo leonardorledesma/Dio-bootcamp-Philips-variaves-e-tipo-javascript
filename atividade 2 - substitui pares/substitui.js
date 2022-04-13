7cf1029daad4e3edf9e2746a9af64aa5165c57da
leonardorledesma <leo.ledesma@gmail.com>
2022-04-13 03:25:34 -0300

    primeiro commit - atividades 1 e 2.

---
+++ atividade 2 - substitui pares/substitui.js
@@ -0,0 +1,17 @@
+
+function substituiPares(array){
+    if (!array) return -1;
+    if (!array.length) return -1;
+
+    for(let i = 0; array.length; i++){
+    if(array[i] === 0){
+    console.log("Você já é zero!")
+    }else if(array[i] % 2 == 0){
+    console.log(`Substituindo ${array[i]} por 0...`)
+    array[i] = 0;
+    }
+    }
+    return array;
+}
+let arr = [1, 5, 22, 7, 24, 0, 90];
+console.log(substituiPares(arr));
